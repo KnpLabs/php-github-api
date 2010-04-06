a5b4067d8f4248ebde5a894578ea83539c63452e
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-06 20:54:53 +0200

    replaced cmp_ok with ok in unit tests to avoid PHP 5.3 segfaults

--- test/unit/objectTest.php
+++ test/unit/objectTest.php
@@ -10,9 +10,9 @@ $t->comment('Test ->listObjectTree');
 
 $tree = $api->listObjectTree('ornicar', 'php-github-api', '691c2ec7fd0b948042047b515886fec40fe76e2b');
 $firstFile = array_pop($tree);
-$t->cmp_ok($firstFile['sha'], '!=', null, 'Tree returned with SHA listings');
+$t->ok($firstFile['sha'] != null, 'Tree returned with SHA listings');
 $blob = $api->showObjectBlob('ornicar', 'php-github-api', '691c2ec7fd0b948042047b515886fec40fe76e2b', 'CHANGELOG');
 print_r($blob);
 $t->is($blob['name'], 'CHANGELOG', 'Returned CHANGELOG blob');
 $blobs = $api->listObjectBlobs('ornicar', 'php-github-api', '691c2ec7fd0b948042047b515886fec40fe76e2b');
-$t->cmp_ok(count($blobs), '>', 0, 'Returned array of blobs');
+$t->ok(count($blobs) > 0, 'Returned array of blobs');
