40271fdd087a80c15b2a3cbadc435eb83c696629
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-08 01:19:27 +0200

    refactored phpGitHubApiRequest, extracted class phpGitHubApiRequestException

--- lib/phpGitHubApiRequest.php
+++ lib/phpGitHubApiRequest.php
@@ -1,12 +1,13 @@
 <?php
 
+require_once(dirname(__FILE__).'/phpGitHubApiRequestException.php');
+
 /**
  * Performs requests on GitHub API. API documentation should be self-explanatory.
  *
  * @author	Thibault Duplessis <thibault.duplessis at gmail dot com>
  * @license	MIT License
  */
-
 class phpGitHubApiRequest
 {
   protected
@@ -120,27 +121,28 @@ class phpGitHubApiRequest
 
     $this->debug('send request: '.$completeUrl);
 
-    $options[CURLOPT_URL] = $completeUrl;
-    $options[CURLOPT_PORT] = $this->options['http_port'];
-    $options[CURLOPT_USERAGENT] = $this->options['user_agent'];
-    $options[CURLOPT_FOLLOWLOCATION] = true;
-    $options[CURLOPT_RETURNTRANSFER] = true;
-    $options[CURLOPT_TIMEOUT] = $this->options['timeout'];
+    $curlOptions = array(
+      CURLOPT_URL             => $completeUrl,
+      CURLOPT_PORT            => $this->options['http_port'],
+      CURLOPT_USERAGENT       => $this->options['user_agent'],
+      CURLOPT_FOLLOWLOCATION  => true,
+      CURLOPT_RETURNTRANSFER  => true,
+      CURLOPT_TIMEOUT         => $this->options['timeout']
+    );
 
     if (!empty($parameters) && 'POST' === $httpMethod)
     {
-      $options[CURLOPT_POST] = true;
-      $options[CURLOPT_POSTFIELDS] = $queryString;
+      $curlOptions[CURLOPT_POST]        = true;
+      $curlOptions[CURLOPT_POSTFIELDS]  = $queryString;
     }
 
     $curl = curl_init();
 
-    curl_setopt_array($curl, $options);
+    curl_setopt_array($curl, $curlOptions);
 
-    $response = curl_exec($curl);
-    $headers = curl_getinfo($curl);
-
-    $errorNumber = curl_errno($curl);
+    $response     = curl_exec($curl);
+    $headers      = curl_getinfo($curl);
+    $errorNumber  = curl_errno($curl);
     $errorMessage = curl_error($curl);
 
     curl_close($curl);
@@ -164,75 +166,5 @@ class phpGitHubApiRequest
     {
       print $message."\n";
     }
-  }
-}
-
-/**
- * Request communication error
- */
-class phpGitHubApiRequestException extends Exception
-{
-  /**
-   * Http header-codes
-   * @var  array
-   */
-  static protected $statusCodes = array(
-      0 => 'OK',
-    100 => 'Continue',
-    101 => 'Switching Protocols',
-    200 => 'OK',
-    201 => 'Created',
-    202 => 'Accepted',
-    203 => 'Non-Authoritative Information',
-    204 => 'No Content',
-    205 => 'Reset Content',
-    206 => 'Partial Content',
-    300 => 'Multiple Choices',
-    301 => 'Moved Permanently',
-    302 => 'Found',
-    303 => 'See Other',
-    304 => 'Not Modified',
-    305 => 'Use Proxy',
-    306 => '(Unused)',
-    307 => 'Temporary Redirect',
-    400 => 'Bad Request',
-    401 => 'Unauthorized',
-    402 => 'Payment Required',
-    403 => 'Forbidden',
-    404 => 'Not Found',
-    405 => 'Method Not Allowed',
-    406 => 'Not Acceptable',
-    407 => 'Proxy Authentication Required',
-    408 => 'Request Timeout',
-    409 => 'Conflict',
-    411 => 'Length Required',
-    412 => 'Precondition Failed',
-    413 => 'Request Entity Too Large',
-    414 => 'Request-URI Too Long',
-    415 => 'Unsupported Media Type',
-    416 => 'Requested Range Not Satisfiable',
-    417 => 'Expectation Failed',
-    500 => 'Internal Server Error',
-    501 => 'Not Implemented',
-    502 => 'Bad Gateway',
-    503 => 'Service Unavailable',
-    504 => 'Gateway Timeout',
-    505 => 'HTTP Version Not Supported'
-  );
-
-  /**
-   * Default constructor
-   *
-   * @param  string $message
-   * @param  int $code
-   */
-  public function __construct($message = null, $code = null)
-  {
-    if (is_null($message) && !is_null($code) && array_key_exists((int) $code, self::$statusCodes))
-    {
-      $message = sprintf('HTTP %d: %s', $code, self::$statusCodes[(int) $code]);
-    }
-
-    parent::__construct($message, $code);
   }
 }
