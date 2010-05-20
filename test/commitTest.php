1a6cfc9ee7f7fd385795d322e34ad049f1d4f31f
ornicar <thibault.duplessis@gmail.com>
2010-05-20 12:14:25 +0200

    Add unit tests for new method ->getCommit()

--- test/commitTest.php
+++ test/commitTest.php
@@ -3,7 +3,7 @@
 require_once dirname(__FILE__).'/vendor/lime.php';
 require_once dirname(__FILE__).'/../lib/phpGitHubApi.php';
 
-$t = new lime_test(4);
+$t = new lime_test(5);
 
 $username = 'ornicar';
 $repo     = 'php-github-api';
@@ -21,6 +21,8 @@ $firstCommit = array_pop($commits);
 
 $t->ok(isset($firstCommit['message']), 'Found master commits');
 
+$t->comment('Test ->getFileCommits');
+
 $commits = $api->getCommitApi()->getFileCommits($username, $repo, $branch, 'README');
 
 $t->is_deeply($api->listFileCommits($username, $repo, $branch, 'README'), $commits, 'Both new and BC syntax work');
@@ -28,3 +30,9 @@ $t->is_deeply($api->listFileCommits($username, $repo, $branch, 'README'), $commi
 $firstCommit = array_pop($commits);
 
 $t->is($firstCommit['message'], 'first commit', 'Found master README commits');
+
+$t->comment('Test ->getCommit');
+
+$commit = $api->getCommitApi()->getCommit($username, $repo, '726eac09a3b44411bd86');
+
+$t->is($commit['message'], 'first commit', 'Found commit 726eac09a3b44411bd86');
