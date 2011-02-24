1e24b1cce7d03e355a13437905f2e0da86b0b5c9
ornicar <thibault.duplessis@gmail.com>
2011-02-24 09:20:12 -0800

    Add unit tests for Curl HttpClient driver

---
+++ test/Github/Tests/HttpClient/CurlTest.php
@@ -0,0 +1,65 @@
+<?php
+
+class Github_Tests_HttpClient_CurlTest extends PHPUnit_Framework_TestCase
+{
+    public function testDoRequest()
+    {
+        $url       = 'http://site.com/some/path';
+        $curlResponse = array('headers' => array('http_code' => 200), 'response' => 'hi there', 'errorNumber' => '', 'errorMessage' => '');
+        $options = array('format' => 'text');
+
+        $httpClient = $this->getHttpClientCurlMockBuilder()
+            ->setMethods(array('doCurlCall'))
+            ->getMock();
+        $httpClient->expects($this->once())
+            ->method('doCurlCall')
+            ->will($this->returnValue($curlResponse));
+
+        $responseText = $httpClient->get($url, array(), $options);
+
+        $this->assertEquals($curlResponse['response'], $responseText);
+    }
+
+    public function testDoGetRequestWithParameters()
+    {
+        $url       = 'http://site.com/some/path';
+        $curlResponse = array('headers' => array('http_code' => 200), 'response' => 'hi there', 'errorNumber' => '', 'errorMessage' => '');
+        $params = array('a' => 'b');
+        $options = array('format' => 'text');
+
+        $httpClient = $this->getHttpClientCurlMockBuilder()
+            ->setMethods(array('doCurlCall'))
+            ->getMock();
+        $httpClient->expects($this->once())
+            ->method('doCurlCall')
+            ->will($this->returnValue($curlResponse));
+
+        $responseText = $httpClient->get($url, $params, $options);
+
+        $this->assertEquals($curlResponse['response'], $responseText);
+    }
+
+    public function testDoPostRequestWithParameters()
+    {
+        $url       = 'http://site.com/some/path';
+        $curlResponse = array('headers' => array('http_code' => 200), 'response' => 'hi there', 'errorNumber' => '', 'errorMessage' => '');
+        $params = array('a' => 'b');
+        $options = array('format' => 'text');
+
+        $httpClient = $this->getHttpClientCurlMockBuilder()
+            ->setMethods(array('doCurlCall'))
+            ->getMock();
+        $httpClient->expects($this->once())
+            ->method('doCurlCall')
+            ->will($this->returnValue($curlResponse));
+
+        $responseText = $httpClient->post($url, $params, $options);
+
+        $this->assertEquals($curlResponse['response'], $responseText);
+    }
+
+    protected function getHttpClientCurlMockBuilder()
+    {
+        return $this->getMockBuilder('Github_HttpClient_Curl');
+    }
+}
