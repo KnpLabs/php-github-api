58bc417b5c2c48e88ccabe90d3f1278686ce22eb
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-06-28 11:53:42 +0200

    functional test for gist api: get by id

--- test/Github/Tests/Functional/GistTest.php
+++ test/Github/Tests/Functional/GistTest.php
@@ -9,7 +9,7 @@ class GistTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function shouldRetrieveGistByUser()
+    public function shouldRetrieveGistsListByUser()
     {
         $username = 'KnpLabs';
 
@@ -25,4 +25,24 @@ class GistTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('user', $gist);        
         $this->assertEquals('KnpLabs', $gist['user']['login']);        
     }
+    
+    /**
+     * @test
+     */
+    public function shouldRetrieveGistById()
+    {
+        $id = 1;
+
+        $github = new Client();
+        $gist = $github->getGistApi()->getGist($id);
+
+        $this->assertArrayHasKey('url', $gist);        
+        $this->assertArrayHasKey('files', $gist);        
+        $this->assertArrayHasKey('comments', $gist);        
+        $this->assertArrayHasKey('created_at', $gist);        
+        $this->assertArrayHasKey('updated_at', $gist);        
+        $this->assertArrayHasKey('user', $gist);        
+        $this->assertArrayHasKey('gistfile1.txt', $gist['files']);        
+        $this->assertEquals('schacon', $gist['user']['login']);        
+    }    
 }
