e9a3e2d0bde5f311f20b1b031ce90a76d60a58fb
Joseph Bielawski <stloyd@gmail.com>
2013-03-17 09:10:06 +0100

    Fix CS from previous merge

--- lib/Github/Api/Repository/Contents.php
+++ lib/Github/Api/Repository/Contents.php
@@ -17,9 +17,9 @@ class Contents extends AbstractApi
      * Get content of README file in a repository
      * @link http://developer.github.com/v3/repos/contents/
      *
-     * @param  string  $username         the user who owns the repository
-     * @param  string  $repository       the name of the repository
-     * @param  string  $reference        reference to a branch or commit
+     * @param  string       $username    the user who owns the repository
+     * @param  string       $repository  the name of the repository
+     * @param  null|string  $reference   reference to a branch or commit
      *
      * @return array                     information for README file
      */
@@ -34,10 +34,10 @@ class Contents extends AbstractApi
      * Get contents of any file or directory in a repository
      * @link http://developer.github.com/v3/repos/contents/
      *
-     * @param  string  $username         the user who owns the repository
-     * @param  string  $repository       the name of the repository
-     * @param  string  $path             path to file or directory
-     * @param  string  $reference        reference to a branch or commit
+     * @param  string       $username    the user who owns the repository
+     * @param  string       $repository  the name of the repository
+     * @param  null|string  $path        path to file or directory
+     * @param  null|string  $reference   reference to a branch or commit
      *
      * @return array                     information for file | information for each item in directory
      */
@@ -57,10 +57,10 @@ class Contents extends AbstractApi
      * Get content of archives in a repository
      * @link http://developer.github.com/v3/repos/contents/
      *
-     * @param  string  $username         the user who owns the repository
-     * @param  string  $repository       the name of the repository
-     * @param  string  $format           format of archive: tarball or zipball
-     * @param  string  $reference        reference to a branch or commit
+     * @param  string       $username    the user who owns the repository
+     * @param  string       $repository  the name of the repository
+     * @param  string       $format      format of archive: tarball or zipball
+     * @param  null|string  $reference   reference to a branch or commit
      *
      * @return array                     information for archives
      */
@@ -78,12 +78,12 @@ class Contents extends AbstractApi
     /**
      * Get the contents of a file in a repository
      *
-     * @param  string  $username         the user who owns the repository
-     * @param  string  $repository       the name of the repository
-     * @param  string  $path             path to file
-     * @param  string  $reference        reference to a branch or commit
+     * @param  string       $username    the user who owns the repository
+     * @param  string       $repository  the name of the repository
+     * @param  string       $path        path to file
+     * @param  null|string  $reference   reference to a branch or commit
      *
-     * @return string|null               content of file, or null in case of base64_decode failure
+     * @return null|string               content of file, or null in case of base64_decode failure
      *
      * @throws InvalidArgumentException  If $path is not a file or if its encoding is different from base64
      * @throws ErrorException            If $path doesn't include a 'content' index
@@ -91,7 +91,7 @@ class Contents extends AbstractApi
     public function download($username, $repository, $path, $reference = null)
     {
         $file = $this->show($username, $repository, $path, $reference);
-        
+
         if (!isset($file['type']) || 'file' !== $file['type']) {
             throw new InvalidArgumentException(sprintf('Path "%s" is not a file.', $path));
         }
@@ -101,8 +101,10 @@ class Contents extends AbstractApi
         }
 
         if (!isset($file['encoding'])) {
-            throw new InvalidArgumentException(sprintf('Can\t decode contents of file "%s" as no encoding is defined.', $path));
-        } elseif ('base64' !== $file['encoding']) {
+            throw new InvalidArgumentException(sprintf('Can\'t decode content of file "%s", as no encoding is defined.', $path));
+        }
+
+        if ('base64' !== $file['encoding']) {
             throw new InvalidArgumentException(sprintf('Encoding "%s" of file "%s" is not supported.', $file['encoding'], $path));
         }
 
