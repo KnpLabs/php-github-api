76c1b46be387bd1dedbbc6e5de3a3142a1843107
Mitchel Verschoof <mitchel@future500.nl>
2013-05-27 16:28:11 +0200

    removed debug

--- lib/Github/ResultPaginator.php
+++ lib/Github/ResultPaginator.php
@@ -14,43 +14,54 @@ class ResultPaginator
 
     protected $pagination = null;
 
-    public function __construct( Client $client ) {
+    public function __construct( Client $client )
+    {
         $this->client = $client;
     }
 
-    public function fetchAll( ApiInterface $api, $method, $parameters ) {
+    public function fetchAll( ApiInterface $api, $method, $parameters )
+    {
         $result = array();
-        $result = call_user_func_array( array($api,$method), $parameters);
+        $result = call_user_func_array(array($api, $method), $parameters);
+
         $this->postFetch();
 
         while ($this->hasNext()) {
             $result = array_merge($result, $this->fetchNext());
         }
+
         return $result;
     }
 
-    protected function postFetch() {
+    protected function postFetch()
+    {
         $this->pagination = $this->client->getHttpClient()->getLastResponse()->getPagination();
-        var_dump($this->pagination);
     }
 
-    public function fetchNext() {
+    public function fetchNext()
+    {
         $result = $this->client->getHttpClient()->get($this->pagination['next']);
+
         $this->postFetch();
+
         return $result->getContent();
     }
 
-    public function hasNext() {
+    public function hasNext()
+    {
         if (!empty($this->pagination) and isset($this->pagination['next'])) {
             return true;
         }
+
         return false;
     }
 
-    public function hasPrevious() {
+    public function hasPrevious()
+    {
         if (!empty($this->pagination) and isset($this->pagination['previous'])) {
             return true;
         }
+
         return false;
     }
 
