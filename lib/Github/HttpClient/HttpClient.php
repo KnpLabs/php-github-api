79889a66c4d02749b48dfb51b94e6efe06126838
Grégoire Pineau <lyrixx@lyrixx.info>
2014-06-19 17:02:26 +0200

    Removed double header from a request

--- lib/Github/HttpClient/HttpClient.php
+++ lib/Github/HttpClient/HttpClient.php
@@ -135,7 +135,6 @@ class HttpClient implements HttpClientInterface
     public function request($path, $body = null, $httpMethod = 'GET', array $headers = array(), array $options = array())
     {
         $request = $this->createRequest($httpMethod, $path, $body, $headers, $options);
-        $request->addHeaders($headers);
 
         try {
             $response = $this->client->send($request);
