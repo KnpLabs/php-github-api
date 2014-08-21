81aa33e822c949e2a58d2aee8bb510f64ac643e6
James Brooks <james@bluebaytravel.co.uk>
2014-08-21 10:12:35 +0100

    Closes #177 - use the magic __call method for IDE completion

--- lib/Github/Client.php
+++ lib/Github/Client.php
@@ -10,6 +10,34 @@ use Github\HttpClient\HttpClientInterface;
 /**
  * Simple yet very cool PHP GitHub client
  *
+ * @method Api\CurrentUser currentUser()
+ * @method Api\CurrentUser me()
+ * @method Api\Enterprise ent()
+ * @method Api\Enterprise enterprise()
+ * @method Api\GitData git()
+ * @method Api\GitData gitData()
+ * @method Api\Gists gist()
+ * @method Api\Gists gists()
+ * @method Api\Issue issue()
+ * @method Api\Issue issues()
+ * @method Api\Markdown markdown()
+ * @method Api\Organization organization()
+ * @method Api\Organization organizations()
+ * @method Api\PullRequest pr()
+ * @method Api\PullRequest pullRequest()
+ * @method Api\PullRequest pullRequests()
+ * @method Api\Repo repo()
+ * @method Api\Repo repos()
+ * @method Api\Repo repository()
+ * @method Api\Repo repositories()
+ * @method Api\Organization team()
+ * @method Api\Organization teams()
+ * @method Api\User user()
+ * @method Api\User users()
+ * @method Api\Authorizations authorization()
+ * @method Api\Authorizations authorizations()
+ * @method Api\Meta meta()
+ *
  * @author Joseph Bielawski <stloyd@gmail.com>
  *
  * Website: http://github.com/KnpLabs/php-github-api
@@ -273,5 +301,87 @@ class Client
     public function getSupportedApiVersions()
     {
         return array('v3', 'beta');
+    }
+
+    /**
+     * @param string $name
+     * 
+     * @return ApiInterface
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __call($name) {
+        switch ($name) {
+            case 'me':
+            case 'currentUser':
+                $api = new Api\CurrentUser($this);
+                break;
+
+            case 'ent':
+            case 'enterprise':
+                $api = new Api\Enterprise($this);
+                break;
+
+            case 'git':
+            case 'gitData':
+                $api = new Api\GitData($this);
+                break;
+
+            case 'gist':
+            case 'gists':
+                $api = new Api\Gists($this);
+                break;
+
+            case 'issue':
+            case 'issues':
+                $api = new Api\Issue($this);
+                break;
+
+            case 'markdown':
+                $api = new Api\Markdown($this);
+                break;
+
+            case 'organization':
+            case 'organizations':
+                $api = new Api\Organization($this);
+                break;
+
+            case 'pr':
+            case 'pullRequest':
+            case 'pullRequests':
+                $api = new Api\PullRequest($this);
+                break;
+
+            case 'repo':
+            case 'repos':
+            case 'repository':
+            case 'repositories':
+                $api = new Api\Repo($this);
+                break;
+
+            case 'team':
+            case 'teams':
+                $api = new Api\Organization\Teams($this);
+                break;
+
+            case 'user':
+            case 'users':
+                $api = new Api\User($this);
+                break;
+
+            case 'authorization':
+            case 'authorizations':
+                $api = new Api\Authorizations($this);
+                break;
+
+            case 'meta':
+                $api = new Api\Meta($this);
+                break;
+
+            default:
+                throw new InvalidArgumentException(sprintf('Undefined api instance called: "%s"', $name));
+        }
+
+        return $api;
     }
 }
