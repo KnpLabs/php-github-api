3dd65b7bae223030c102b9ca985daa48e451c58a
Greg Payne <greg.payne@gmail.com>
2014-09-08 15:00:59 -0500

    Split find into 4 methods.

--- lib/Github/Api/Search.php
+++ lib/Github/Api/Search.php
@@ -16,23 +16,65 @@ use Github\Exception\MissingArgumentException;
  */
 class Search extends AbstractApi
 {
+
     /**
-     * Search by filter (q)
-     * @link http://developer.github.com/v3/search/
+     * Search repositories by filter (q)
+     * @link https://developer.github.com/v3/search/#search-repositories
      *
-     * @param string $type       the search type
+     * @param string $q          the filter
+     * @param string $sort       the sort field
+     * @param string $order      asc/desc
+     *
+     * @return array list of repositories found
+     */
+    public function repositories($q, $sort = 'updated', $order = 'desc')
+    {
+        return $this->get("/search/repositories", array('q' => $q, 'sort' => $sort, 'order' => $order));
+    }
+
+    /**
+     * Search issues by filter (q)
+     * @link https://developer.github.com/v3/search/#search-issues
+     *
      * @param string $q          the filter
      * @param string $sort       the sort field
      * @param string $order      asc/desc
      *
      * @return array list of issues found
      */
-    public function find($type = 'issues', $q = '', $sort = 'updated', $order = 'desc')
+    public function issues($q, $sort = 'updated', $order = 'desc')
     {
-        if (!in_array($type, array('issues', 'repositories', 'code', 'users'))) {
-            $type = 'issues';
-        }
-        return $this->get("/search/$type", array('q' => $q, 'sort' => $sort, 'order' => $order));
+        return $this->get("/search/issues", array('q' => $q, 'sort' => $sort, 'order' => $order));
+    }
+
+    /**
+     * Search code by filter (q)
+     * @link https://developer.github.com/v3/search/#search-code
+     *
+     * @param string $q          the filter
+     * @param string $sort       the sort field
+     * @param string $order      asc/desc
+     *
+     * @return array list of code found
+     */
+    public function code($q, $sort = 'updated', $order = 'desc')
+    {
+        return $this->get("/search/code", array('q' => $q, 'sort' => $sort, 'order' => $order));
+    }
+
+    /**
+     * Search users by filter (q)
+     * @link https://developer.github.com/v3/search/#search-users
+     *
+     * @param string $q          the filter
+     * @param string $sort       the sort field
+     * @param string $order      asc/desc
+     *
+     * @return array list of users found
+     */
+    public function users($q, $sort = 'updated', $order = 'desc')
+    {
+        return $this->get("/search/users", array('q' => $q, 'sort' => $sort, 'order' => $order));
     }
 
 }
