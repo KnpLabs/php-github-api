daad20d4cda8b4f2d32899839b0200eeb3684d78
guillermo-fisher <guillermo.fisher@dominionenterprises.com>
2014-10-31 10:01:49 -0400

    Removed the unneeded docblocks.

--- lib/Github/Api/Enterprise/ManagementConsole.php
+++ lib/Github/Api/Enterprise/ManagementConsole.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * This file is part of the php-github-api package.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 namespace Github\Api\Enterprise;
 
