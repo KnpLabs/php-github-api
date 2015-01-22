9ae67e381351637ff74d852731b963139ba2c399
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2015-01-22 13:28:52 +0100

    In method arguments and method call, there MUST NOT be a space before each comma and there MUST be one space after each comma.

--- test/Github/Tests/Functional/MarkdownTest.php
+++ test/Github/Tests/Functional/MarkdownTest.php
@@ -22,7 +22,7 @@ class MarkdownTest extends TestCase
 
         $input  = 'Hello world KnpLabs/KnpBundles#1 **cool**, and #1!';
         $output = '<p>Hello world <a href="https://github.com/KnpLabs/KnpBundles/issues/1" class="issue-link" title="Display docs">KnpLabs/KnpBundles#1</a> <strong>cool</strong>, and <a href="https://github.com/KnpLabs/KnpMenu/issues/1" class="issue-link" title="Limitation when using the TwigRenderer from elsewhere than a Twig template">#1</a>!</p>';
-        $html   = $api->render($input, 'gfm' , 'KnpLabs/KnpMenu');
+        $html   = $api->render($input, 'gfm', 'KnpLabs/KnpMenu');
 
         $this->assertEquals($output, $html);
     }
