7e5e260f88d7a015d29c9d3cc0b267cec7eaeba4
Dennis de Greef <github@link0.net>
2015-02-08 14:12:42 +0100

    Implemented notifications from github API

---
+++ lib/Github/Api/Activity/Notification.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Github\Api\Activity;
+
+use DateTime;
+use DateTimeInterface;
+use Github\Api\AbstractApi;
+
+/**
+ * API for accessing Notifications from your Git/Github repositories.
+ *
+ * Important! You have to be authenticated to perform these methods
+ *
+ * @link   https://developer.github.com/v3/activity/notifications/
+ * @author Dennis de Greef <github@link0.net>
+ */
+class Notification extends AbstractApi
+{
+    /**
+     * Get a listing of a notifications
+     * @link https://developer.github.com/v3/activity/notifications/
+     *
+     * @param bool $includingRead
+     * @param bool $participating
+     * @param null $since
+     *
+     * @return array array of notifications
+     */
+    public function all($includingRead = false, $participating = false, $since = null)
+    {
+        $parameters = array(
+            'all' => $includingRead,
+            'participating' => $participating
+        );
+
+        if($since !== null) {
+            $parameters['since'] = $since;
+        }
+
+        return $this->get('notifications', $parameters);
+    }
+
+    /**
+     * Marks all notifications as read from the current date
+     * Optionally give DateTimeInterface to mark as read before that date
+     *
+     * @link https://developer.github.com/v3/activity/notifications/#mark-as-read
+     *
+     * @param DateTimeInterface $since
+     *
+     */
+    public function markRead(DateTimeInterface $since = null)
+    {
+        $parameters = array();
+
+        if($since !== null) {
+            $parameters['last_read_at'] = $since->format(DateTime::ISO8601);
+        }
+
+        $this->put('notifications', $parameters);
+    }
+}
