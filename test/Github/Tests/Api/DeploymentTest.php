5d94a88f0b4233aa176948b22528df3ab42a2d38
lol768 <lol768@lol768.com>
2015-02-20 20:42:52 +0000

    Add some more tests

--- test/Github/Tests/Api/DeploymentTest.php
+++ test/Github/Tests/Api/DeploymentTest.php
@@ -9,14 +9,81 @@ class DeploymentTest extends TestCase
      */
     public function shouldCreateDeployment()
     {
-        /** @var \Github\Api\Deployment $api */
         $api = $this->getApiMock();
         $deploymentData = array("ref" => "fd6a5f9e5a430dddae8d6a8ea378f913d3a766f9");
         $api->expects($this->once())
             ->method('post')
-            ->with('/repos/KnpLabs/php-github-api/deployments', $deploymentData);
+            ->with('repos/KnpLabs/php-github-api/deployments', $deploymentData);
 
         $api->create("KnpLabs", "php-github-api", $deploymentData);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetAllDeployments()
+    {
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('repos/KnpLabs/php-github-api/deployments');
+
+        $api->all("KnpLabs", "php-github-api");
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetAllDeploymentsWithFilterParameters()
+    {
+        $api = $this->getApiMock();
+        $filterData = ["foo" => "bar", "bar" => "foo"];
+
+        $api->expects($this->once())
+            ->method('get')
+            ->with('repos/KnpLabs/php-github-api/deployments', $filterData);
+
+        $api->all("KnpLabs", "php-github-api", $filterData);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateStatusUpdate()
+    {
+        $api = $this->getApiMock();
+        $statusData = ["state" => "pending", "description" => "waiting to start"];
+
+        $api->expects($this->once())
+            ->method('post')
+            ->with('repos/KnpLabs/php-github-api/deployments/1/statuses', $statusData);
+
+        $api->updateStatus("KnpLabs", "php-github-api", 1, $statusData);
+    }
+
+    /**
+     * @test
+     * @expectedException GitHub\Exception\MissingArgumentException
+     */
+    public function shouldRejectStatusUpdateWithoutStateField()
+    {
+        $api = $this->getApiMock();
+        $statusData = [ "description" => "waiting to start"];
+
+        $api->updateStatus("KnpLabs", "php-github-api", 1, $statusData);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetAllStatuses()
+    {
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('repos/KnpLabs/php-github-api/deployments/1/statuses');
+
+        $api->getStatuses("KnpLabs", "php-github-api", 1);
     }
 
     protected function getApiClass()
