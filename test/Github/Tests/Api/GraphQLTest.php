d31ef0b4eb2a22e21f46c1b32a6a175712ac1511
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-01-26 09:47:53 +0100

    Add Test

---
+++ test/Github/Tests/Api/GraphQLTest.php
@@ -0,0 +1,25 @@
+class GraphQLTest extends TestCase
+{
+    // ...
+
+    /**
+     * @test
+     */
+    public function shouldShowGistComment()
+    {
+        $api = $this->getApiMock();
+
+        $api->expects($this->once())
+            ->method('post')
+            ->with('/graphql', 'bar')
+            ->will($this->returnValue('foo'));
+
+        $result = $api->graphql('bar');
+        $this->assertEquals('foo', $result);
+    }
+
+    protected function getApiClass()
+    {
+        return \Github\Api\GraphQL::class;
+    }
+}
