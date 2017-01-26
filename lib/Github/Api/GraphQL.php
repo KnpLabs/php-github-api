25d30c1375d75509d6f48b235114dd383dc9192f
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-01-26 09:36:28 +0100

    Add Early Access note

--- lib/Github/Api/GraphQL.php
+++ lib/Github/Api/GraphQL.php
@@ -5,6 +5,7 @@ namespace Github\Api;
 /**
  * GraphQL API.
  *
+ * Part of the Github API Early-Access Program
  * @link   https://developer.github.com/early-access/graphql/
  * @author Miguel Piedrafita <soy@miguelpiedrafita.com>
  */
