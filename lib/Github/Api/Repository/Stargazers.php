0ae186e7f3264258c5bac1b77c45f1bbe95c5721
Jeroen Thora <jeroenthora@gmail.com>
2017-03-25 16:16:01 +0100

    Make all configure methods chainable

--- lib/Github/Api/Repository/Stargazers.php
+++ lib/Github/Api/Repository/Stargazers.php
@@ -20,12 +20,15 @@ class Stargazers extends AbstractApi
      * @see https://developer.github.com/v3/activity/starring/#alternative-response-with-star-creation-timestamps
      *
      * @param string $bodyType
+     *
+     * @return self
      */
     public function configure($bodyType = null)
     {
         if ('star' === $bodyType) {
             $this->acceptHeaderValue = sprintf('application/vnd.github.%s.star+json', $this->client->getApiVersion());
         }
+
         return $this;
     }
 
