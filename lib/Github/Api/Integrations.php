a0a1f0e3c134ba9898baa32df13d2cec5d275f83
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-05-22 16:18:21 +0200

    Keep BC

---
+++ lib/Github/Api/Integrations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Github\Api;
+
+/**
+ * @deprecated
+ * @link   https://developer.github.com/v3/apps/
+ * @author Nils Adermann <naderman@naderman.de>
+ */
+class Integrations extends Apps
+{
+
+}
