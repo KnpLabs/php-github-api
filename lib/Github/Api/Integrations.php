aa27c9c0b8af9ae3eea4457d79d3c71d286e7b0a
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-05-23 09:16:15 +0200

    Add deprecation notice

--- lib/Github/Api/Integrations.php
+++ lib/Github/Api/Integrations.php
@@ -2,8 +2,10 @@
 
 namespace Github\Api;
 
+@trigger_error('The '.__NAMESPACE__.'\Integrations class is deprecated. Use the '.__NAMESPACE__.'\Apps class instead.', E_USER_DEPRECATED);
+
 /**
- * @deprecated
+ * @deprecated Use the Apps class
  * @link   https://developer.github.com/v3/apps/
  * @author Nils Adermann <naderman@naderman.de>
  */
