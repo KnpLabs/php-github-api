e45fba2a98b43b116966d66a65f1fcb7d7b10786
Luke Rodgers <lukerodgers90@gmail.com>
2017-08-10 21:08:02 +0100

    Add phpdoc to Collaborators api

--- lib/Github/Api/Repository/Collaborators.php
+++ lib/Github/Api/Repository/Collaborators.php
@@ -10,21 +10,54 @@ use Github\Api\AbstractApi;
  */
 class Collaborators extends AbstractApi
 {
+    /**
+     * @link https://developer.github.com/v3/repos/collaborators/#list-collaborators
+     *
+     * @param $username
+     * @param $repository
+     * @param array $params
+     * @return array|string
+     */
     public function all($username, $repository, array $params = [])
     {
         return $this->get('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/collaborators', $params);
     }
 
+    /**
+     * @link https://developer.github.com/v3/repos/collaborators/#check-if-a-user-is-a-collaborator
+     *
+     * @param $username
+     * @param $repository
+     * @param $collaborator
+     * @return array|string
+     */
     public function check($username, $repository, $collaborator)
     {
         return $this->get('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/collaborators/'.rawurlencode($collaborator));
     }
 
+    /**
+     * @link https://developer.github.com/v3/repos/collaborators/#add-user-as-a-collaborator
+     *
+     * @param $username
+     * @param $repository
+     * @param $collaborator
+     * @param array $params
+     * @return array|string
+     */
     public function add($username, $repository, $collaborator, array $params = [])
     {
         return $this->put('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/collaborators/'.rawurlencode($collaborator), $params);
     }
 
+    /**
+     * @link https://developer.github.com/v3/repos/collaborators/#remove-user-as-a-collaborator
+     *
+     * @param $username
+     * @param $repository
+     * @param $collaborator
+     * @return array|string
+     */
     public function remove($username, $repository, $collaborator)
     {
         return $this->delete('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/collaborators/'.rawurlencode($collaborator));
