3667cee456811238c4a31211e0340aa871defd68
Nils Adermann <naderman@naderman.de>
2017-12-12 20:10:29 +0100

    PathPrepend plugin: Only overwrite the request uri if it actually changed (#663)
    * PathPrepend plugin: Only overwrite the request uri if it actually changed
    
    * Add test for pathprepend http plugin
    
    * Formatting
    
    * Updated to phpunit6

---
+++ test/Github/Tests/HttpClient/PathPrependTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Github\Tests\HttpClient\Plugin;
+
+use Github\HttpClient\Plugin\PathPrepend;
+use GuzzleHttp\Psr7\Request;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Nils Adermann <naderman@naderman.de>
+ */
+class PathPrependTest extends TestCase
+{
+    /**
+     * @dataProvider uris
+     */
+    public function testPathIsPrepended($uri, $expectedPath)
+    {
+        $request = new Request('GET', $uri);
+        $plugin = new PathPrepend('/api/v3');
+
+        $newRequest = null;
+        $plugin->handleRequest($request, function ($request) use (&$newRequest) {
+            $newRequest = $request;
+        }, function () {
+            throw new \RuntimeException("Did not expect plugin to call first");
+        });
+
+        $this->assertEquals($expectedPath, $newRequest->getUri()->getPath());
+    }
+
+    public static function uris()
+    {
+        return [
+            ['http://example.com/foo/bar/api', '/api/v3/foo/bar/api'],
+            ['http://example.com/api/v3/foo/bar/api', '/api/v3/foo/bar/api'],
+        ];
+    }
+}
