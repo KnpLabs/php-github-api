d94da3d80c1074cb04a30076e4ba2e50c58d07b1
Mike Milano <coder1@gmail.com>
2018-07-15 11:09:07 -0700

    Search topics implementation (#718)
    * Added API support for searching by topic.
    
    * Added accept header for preview mode.
    
    * Fixed parameter definition style.

--- lib/Github/Api/Search.php
+++ lib/Github/Api/Search.php
@@ -95,4 +95,21 @@ class Search extends AbstractApi
 
         return $this->get('/search/commits', ['q' => $q, 'sort' => $sort, 'order' => $order]);
     }
+
+    /**
+     * Search commits by filter (q).
+     *
+     * @link https://developer.github.com/v3/search/#search-topics
+     *
+     * @param string $q the filter
+     *
+     * @return array
+     */
+    public function topics($q)
+    {
+        //This api is in preview mode, so set the correct accept-header
+        $this->acceptHeaderValue = 'application/vnd.github.mercy-preview+json';
+
+        return $this->get('/search/topics', ['q' => $q]);
+    }
 }
