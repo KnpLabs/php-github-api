938b569e6bd7bfe53c0b824a51950b00b767c46b
Mitchell Davis <mitch@wingmanwebdesign.com.au>
2019-06-10 12:42:53 +1000

    Change DeployKeys::update method to first remove the existing key and then create a new one, in accordance with the supported GitHub APIs

--- test/Github/Tests/Api/Repository/DeployKeysTest.php
+++ test/Github/Tests/Api/Repository/DeployKeysTest.php
@@ -6,153 +6,161 @@ use Github\Tests\Api\TestCase;
 
 class DeployKeysTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function shouldGetAllRepositoryDeployKeys()
-    {
-        $expectedValue = [['name' => 'key']];
+	/**
+	 * @test
+	 */
+	public function shouldGetAllRepositoryDeployKeys()
+	{
+		$expectedValue = [['name' => 'key']];
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('get')
-            ->with('/repos/KnpLabs/php-github-api/keys')
-            ->will($this->returnValue($expectedValue));
+		$api = $this->getApiMock();
+		$api->expects($this->once())
+		    ->method('get')
+		    ->with('/repos/KnpLabs/php-github-api/keys')
+		    ->will($this->returnValue($expectedValue));
 
-        $this->assertEquals($expectedValue, $api->all('KnpLabs', 'php-github-api'));
-    }
+		$this->assertEquals($expectedValue, $api->all('KnpLabs', 'php-github-api'));
+	}
 
-    /**
-     * @test
-     */
-    public function shouldShowDeployKey()
-    {
-        $expectedValue = ['key' => 'somename'];
+	/**
+	 * @test
+	 */
+	public function shouldShowDeployKey()
+	{
+		$expectedValue = ['key' => 'somename'];
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('get')
-            ->with('/repos/KnpLabs/php-github-api/keys/123')
-            ->will($this->returnValue($expectedValue));
+		$api = $this->getApiMock();
+		$api->expects($this->once())
+		    ->method('get')
+		    ->with('/repos/KnpLabs/php-github-api/keys/123')
+		    ->will($this->returnValue($expectedValue));
 
-        $this->assertEquals($expectedValue, $api->show('KnpLabs', 'php-github-api', 123));
-    }
+		$this->assertEquals($expectedValue, $api->show('KnpLabs', 'php-github-api', 123));
+	}
 
-    /**
-     * @test
-     */
-    public function shouldRemoveDeployKey()
-    {
-        $expectedValue = ['someOutput'];
+	/**
+	 * @test
+	 */
+	public function shouldRemoveDeployKey()
+	{
+		$expectedValue = ['someOutput'];
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('delete')
-            ->with('/repos/KnpLabs/php-github-api/keys/123')
-            ->will($this->returnValue($expectedValue));
+		$api = $this->getApiMock();
+		$api->expects($this->once())
+		    ->method('delete')
+		    ->with('/repos/KnpLabs/php-github-api/keys/123')
+		    ->will($this->returnValue($expectedValue));
 
-        $this->assertEquals($expectedValue, $api->remove('KnpLabs', 'php-github-api', 123));
-    }
+		$this->assertEquals($expectedValue, $api->remove('KnpLabs', 'php-github-api', 123));
+	}
 
-    /**
-     * @test
-     * @expectedException \Github\Exception\MissingArgumentException
-     */
-    public function shouldNotCreateDeployKeyWithoutName()
-    {
-        $data = ['config' => 'conf'];
+	/**
+	 * @test
+	 * @expectedException \Github\Exception\MissingArgumentException
+	 */
+	public function shouldNotCreateDeployKeyWithoutName()
+	{
+		$data = ['config' => 'conf'];
 
-        $api = $this->getApiMock();
-        $api->expects($this->never())
-            ->method('post');
+		$api = $this->getApiMock();
+		$api->expects($this->never())
+		    ->method('post');
 
-        $api->create('KnpLabs', 'php-github-api', $data);
-    }
+		$api->create('KnpLabs', 'php-github-api', $data);
+	}
 
-    /**
-     * @test
-     * @expectedException \Github\Exception\MissingArgumentException
-     */
-    public function shouldNotCreateDeployKeyWithoutColor()
-    {
-        $data = ['name' => 'test'];
+	/**
+	 * @test
+	 * @expectedException \Github\Exception\MissingArgumentException
+	 */
+	public function shouldNotCreateDeployKeyWithoutColor()
+	{
+		$data = ['name' => 'test'];
 
-        $api = $this->getApiMock();
-        $api->expects($this->never())
-            ->method('post');
+		$api = $this->getApiMock();
+		$api->expects($this->never())
+		    ->method('post');
 
-        $api->create('KnpLabs', 'php-github-api', $data);
-    }
+		$api->create('KnpLabs', 'php-github-api', $data);
+	}
 
-    /**
-     * @test
-     */
-    public function shouldCreateDeployKey()
-    {
-        $expectedValue = ['key' => 'somename'];
-        $data = ['title' => 'test', 'key' => 'ssh-rsa 1231234232'];
+	/**
+	 * @test
+	 */
+	public function shouldCreateDeployKey()
+	{
+		$expectedValue = ['key' => 'somename'];
+		$data = ['title' => 'test', 'key' => 'ssh-rsa 1231234232'];
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('post')
-            ->with('/repos/KnpLabs/php-github-api/keys', $data)
-            ->will($this->returnValue($expectedValue));
+		$api = $this->getApiMock();
+		$api->expects($this->once())
+		    ->method('post')
+		    ->with('/repos/KnpLabs/php-github-api/keys', $data)
+		    ->will($this->returnValue($expectedValue));
 
-        $this->assertEquals($expectedValue, $api->create('KnpLabs', 'php-github-api', $data));
-    }
+		$this->assertEquals($expectedValue, $api->create('KnpLabs', 'php-github-api', $data));
+	}
 
-    /**
-     * @test
-     * @expectedException \Github\Exception\MissingArgumentException
-     */
-    public function shouldNotUpdateDeployKeyWithoutTitle()
-    {
-        $data = ['key' => 'ssh-rsa 12323213'];
+	/**
+	 * @test
+	 * @expectedException \Github\Exception\MissingArgumentException
+	 */
+	public function shouldNotUpdateDeployKeyWithoutTitle()
+	{
+		$data = ['key' => 'ssh-rsa 12323213'];
 
-        $api = $this->getApiMock();
-        $api->expects($this->never())
-            ->method('patch');
+		$api = $this->getApiMock();
+		$api->expects($this->never())
+		    ->method('remove');
+		$api->expects($this->never())
+		    ->method('create');
 
-        $api->update('KnpLabs', 'php-github-api', 123, $data);
-    }
+		$api->update('KnpLabs', 'php-github-api', 123, $data);
+	}
 
-    /**
-     * @test
-     * @expectedException \Github\Exception\MissingArgumentException
-     */
-    public function shouldNotUpdateDeployKeyWithoutKey()
-    {
-        $data = ['title' => 'test'];
+	/**
+	 * @test
+	 * @expectedException \Github\Exception\MissingArgumentException
+	 */
+	public function shouldNotUpdateDeployKeyWithoutKey()
+	{
+		$data = ['title' => 'test'];
 
-        $api = $this->getApiMock();
-        $api->expects($this->never())
-            ->method('patch');
+		$api = $this->getApiMock();
+		$api->expects($this->never())
+		    ->method('remove');
+		$api->expects($this->never())
+		    ->method('create');
 
-        $api->update('KnpLabs', 'php-github-api', 123, $data);
-    }
+		$api->update('KnpLabs', 'php-github-api', 123, $data);
+	}
 
-    /**
-     * @test
-     */
-    public function shouldUpdateDeployKey()
-    {
-        $expectedValue = ['key' => 'somename'];
-        $data = ['title' => 'test', 'key' => 'ssh-rsa 12312312321...'];
+	/**
+	 * @test
+	 */
+	public function shouldUpdateDeployKey()
+	{
+		$expectedValue = ['key' => 'somename'];
+		$data = ['title' => 'test', 'key' => 'ssh-rsa 12312312321...'];
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('patch')
-            ->with('/repos/KnpLabs/php-github-api/keys/123', $data)
-            ->will($this->returnValue($expectedValue));
+		$api = $this->getApiMock();
+		$api->expects($this->once())
+		    ->method('delete')
+		    ->with('/repos/KnpLabs/php-github-api/keys/123')
+		    ->will($this->returnValue($expectedValue));
+		$api->expects($this->once())
+		    ->method('post')
+		    ->with('/repos/KnpLabs/php-github-api/keys', $data)
+		    ->will($this->returnValue($expectedValue));
 
-        $this->assertEquals($expectedValue, $api->update('KnpLabs', 'php-github-api', 123, $data));
-    }
+		$this->assertEquals($expectedValue, $api->update('KnpLabs', 'php-github-api', 123, $data));
+	}
 
-    /**
-     * @return string
-     */
-    protected function getApiClass()
-    {
-        return \Github\Api\Repository\DeployKeys::class;
-    }
+	/**
+	 * @return string
+	 */
+	protected function getApiClass()
+	{
+		return \Github\Api\Repository\DeployKeys::class;
+	}
 }
