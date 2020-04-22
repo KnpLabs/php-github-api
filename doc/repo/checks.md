3d50b433dbb825bd1dab3261d40c99f09b6686cb
Bob Eagan <bob@synapsestudios.com>
2020-04-22 13:52:58 -0700

    add additional check run methods

--- doc/repo/checks.md
+++ doc/repo/checks.md
@@ -13,7 +13,7 @@ $params = [
   'details_url' => 'https://nimbleci.com/...',
   'output' => {...}
 ];
-$checks = $client->api('repo')->checks()->create('NimbleCI', 'docker-web-tester-behat', $params);
+$check = $client->api('repo')->checks()->create('NimbleCI', 'docker-web-tester-behat', $params);
 ```
 
 ### Update an existing check on a commit
@@ -27,5 +27,34 @@ $params = [
   'details_url' => 'https://nimbleci.com/...',
   'output' => {...}
 ];
-$checks = $client->api('repo')->checks()->create('NimbleCI', 'docker-web-tester-behat', $checkRunId, $params);
+$check = $client->api('repo')->checks()->create('NimbleCI', 'docker-web-tester-behat', $checkRunId, $params);
+```
+
+### List check runs for a Git reference
+
+https://developer.github.com/v3/checks/runs/#list-check-runs-for-a-git-reference
+
+```php
+$params = [
+  'check_name' => 'my check',
+  'status' => 'completed',
+  'filter' => 'latest',
+];
+$checks = $client->api('repo')->checks()->all('NimbleCI', 'docker-web-tester-behat', $ref, $params);
+```
+
+### Get a check run
+
+https://developer.github.com/v3/checks/runs/#get-a-check-run
+
+```php
+$check = $client->api('repo')->checks()->show('NimbleCI', 'docker-web-tester-behat', $checkRunId);
+```
+
+### List check run annotations
+
+https://developer.github.com/v3/checks/runs/#list-check-run-annotations
+
+```php
+$annotations = $client->api('repo')->checks()->annotations('NimbleCI', 'docker-web-tester-behat', $checkRunId);
 ```
--- lib/Github/Api/Repository/Checks.php
+++ lib/Github/Api/Repository/Checks.php
@@ -55,4 +55,56 @@ class Checks extends AbstractApi
 
         return $this->patch('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/check-runs/'.rawurlencode($checkRunId), $params);
     }
+
+    /**
+     * @link https://developer.github.com/v3/checks/runs/#list-check-runs-for-a-git-reference
+     *
+     * @param string $username
+     * @param string $repository
+     * @param string $ref
+     * @param array  $params
+     *
+     * @return array
+     */
+    public function all($username, $repository, $ref, $params = [])
+    {
+        // This api is in preview mode, so set the correct accept-header.
+        $this->acceptHeaderValue = 'application/vnd.github.antiope-preview+json';
+
+        return $this->patch('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/commits/'.rawurlencode($ref).'/check-runs', $params);
+    }
+
+    /**
+     * @link https://developer.github.com/v3/checks/runs/#get-a-check-run
+     *
+     * @param string $username
+     * @param string $repository
+     * @param string $checkRunId
+     *
+     * @return array
+     */
+    public function show($username, $repository, $checkRunId)
+    {
+        // This api is in preview mode, so set the correct accept-header.
+        $this->acceptHeaderValue = 'application/vnd.github.antiope-preview+json';
+
+        return $this->patch('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/check-runs/'.rawurlencode($checkRunId));
+    }
+
+    /**
+     * @link https://developer.github.com/v3/checks/runs/#list-check-run-annotations
+     *
+     * @param string $username
+     * @param string $repository
+     * @param string $checkRunId
+     *
+     * @return array
+     */
+    public function annotations($username, $repository, $checkRunId)
+    {
+        // This api is in preview mode, so set the correct accept-header.
+        $this->acceptHeaderValue = 'application/vnd.github.antiope-preview+json';
+
+        return $this->patch('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/check-runs/'.rawurlencode($checkRunId).'/annotations');
+    }
 }
