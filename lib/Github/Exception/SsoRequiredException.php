9a9bec73a0e374b036204d77e342521c532d0b10
Eirik S. Morland <eirik@morland.no>
2020-07-29 09:15:43 +0200

    phpstan fixes

--- lib/Github/Exception/SsoRequiredException.php
+++ lib/Github/Exception/SsoRequiredException.php
@@ -7,8 +7,14 @@ namespace Github\Exception;
  */
 class SsoRequiredException extends RuntimeException
 {
+    /** @var string */
     private $url;
 
+    /**
+     * @param string $url
+     * @param int $code
+     * @param \Throwable|null $previous
+     */
     public function __construct($url, $code = 0, $previous = null)
     {
         $this->url = $url;
@@ -16,6 +22,9 @@ class SsoRequiredException extends RuntimeException
         parent::__construct('Resource protected by organization SAML enforcement. You must grant your personal token access to this organization.', $code, $previous);
     }
 
+    /**
+     * @return string
+     */
     public function getUrl()
     {
         return $this->url;
