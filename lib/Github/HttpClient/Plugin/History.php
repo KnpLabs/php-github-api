b3c26f5082adfad8f9f7c1a35e191a6519a7d4b7
Jeroen Thora <jeroenthora@gmail.com>
2020-12-20 21:03:03 +0100

    Mark some classes as final

--- lib/Github/HttpClient/Plugin/History.php
+++ lib/Github/HttpClient/Plugin/History.php
@@ -10,6 +10,7 @@ use Psr\Http\Message\ResponseInterface;
  * A plugin to remember the last response.
  *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ * @final since 2.19
  */
 class History implements Journal
 {
