09f49de40ea11c826b316c26c382e6acd27ed4fe
aireal <code@aware3.com>
2021-04-14 21:43:09 -0500

    Get the combined status for a specific reference
    https://docs.github.com/en/rest/reference/repos#get-the-combined-status-for-a-specific-reference

--- lib/Github/Api/Repository/Commits.php
+++ lib/Github/Api/Repository/Commits.php
@@ -30,4 +30,9 @@ class Commits extends AbstractApi
     {
         return $this->get('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/commits/'.rawurlencode($sha));
     }
+    
+    public function status($username, $repository, $ref)
+    {
+        return $this->get('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/commits/'.rawurlencode($ref).'/status');
+    }
 }
