902e9add10ed99f86d35137421ee3aeaa274f667
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2021-04-30 11:22:29 +0800

    Update lib/Github/Api/GraphQL.php
    Co-authored-by: Markus Staab <maggus.staab@googlemail.com>

--- lib/Github/Api/GraphQL.php
+++ lib/Github/Api/GraphQL.php
@@ -18,6 +18,7 @@ class GraphQL extends AbstractApi
     /**
      * @param string $query
      * @param array  $variables
+     * @param string $acceptHeaderValue
      *
      * @return array
      */
