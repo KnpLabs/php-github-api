aeeda1917873d8aaf970f5fbd8aa050529a5e9ed
Nyholm <tobias.nyholm@gmail.com>
2021-05-13 10:04:29 +0200

    Added comment on AbstractApi::$perPage

--- test/Github/Tests/Integration/ResultPagerTest.php
+++ test/Github/Tests/Integration/ResultPagerTest.php
@@ -11,24 +11,6 @@ class ResultPagerTest extends TestCase
 {
     /**
      * @test
-     */
-    public function shouldPaginateGetRequests()
-    {
-        $repositoriesApi = $this->client->api('user');
-        $repositoriesApi->setPerPage(10);
-
-        $pager = $this->createPager();
-
-        $repositories = $pager->fetch($repositoriesApi, 'repositories', ['KnpLabs']);
-        $this->assertCount(10, $repositories);
-
-        $repositoriesApi->setPerPage(20);
-        $repositories = $pager->fetch($repositoriesApi, 'repositories', ['KnpLabs']);
-        $this->assertCount(20, $repositories);
-    }
-
-    /**
-     * @test
      *
      * response in a search api has different format:
      *
@@ -43,10 +25,8 @@ class ResultPagerTest extends TestCase
     public function shouldGetAllResultsFromSearchApi()
     {
         $searchApi = $this->client->search();
-        $searchApi->setPerPage(10);
 
         $pager = $this->createPager();
-
         $users = $pager->fetch($searchApi, 'users', ['location:Kyiv']);
         $this->assertCount(10, $users);
     }
