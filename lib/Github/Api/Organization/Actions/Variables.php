ff0fb37ddab2f78043528bf6efffb399d121763d
Froxz <sergio11of@gmail.com>
2023-03-09 15:08:01 +0200

    Added Org & Repository varaibles

---
+++ lib/Github/Api/Organization/Actions/Variables.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Github\Api\Organization\Actions;
+
+use Github\Api\AbstractApi;
+use Github\Exception\MissingArgumentException;
+
+/**
+ * @link https://docs.github.com/en/rest/reference/actions#variables
+ */
+class Variables extends AbstractApi
+{
+    /**
+     * @link https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28#list-organization-variables
+     *
+     * @param string $organization
+     *
+     * @return array|string
+     */
+    public function all(string $organization)
+    {
+        return $this->get('/orgs/'.rawurlencode($organization).'/actions/variables');
+    }
+
+    /**
+     * @link https://docs.github.com/en/rest/reference/actions#get-an-organization-secret
+     *
+     * @param string $organization
+     * @param string $variableName
+     *
+     * @return array|string
+     */
+    public function show(string $organization, string $variableName)
+    {
+        return $this->get('/orgs/'.rawurlencode($organization).'/actions/variables/'.rawurlencode($variableName));
+    }
+
+    /**
+     * @link https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28#create-an-organization-variable
+     *
+     * @param string $organization
+     * @param array  $parameters
+     *
+     * @throws MissingArgumentException
+     *
+     * @return array|string
+     */
+    public function create(string $organization, array $parameters)
+    {
+        if (!isset($parameters['name'])) {
+            throw new MissingArgumentException(['name']);
+        }
+
+        if (!isset($parameters['value'])) {
+            throw new MissingArgumentException(['value']);
+        }
+
+        if (!isset($parameters['visibility'])) {
+            throw new MissingArgumentException(['visibility']);
+        }
+
+        return $this->post('/orgs/'.rawurlencode($organization).'/actions/variables', $parameters);
+    }
+
+    /**
+     * @link https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28#update-an-organization-variable
+     *
+     * @param string $organization
+     * @param string $variableName
+     * @param array  $parameters
+     *
+     * @return array|string
+     */
+    public function update(string $organization, string $variableName, array $parameters = [])
+    {
+        return $this->patch('/orgs/'.rawurlencode($organization).'/actions/variables/'.rawurlencode($variableName), $parameters);
+    }
+
+    /**
+     * @link https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28#delete-an-organization-variable
+     *
+     * @param string $organization
+     * @param string $variableName
+     *
+     * @return array|string
+     */
+    public function remove(string $organization, string $variableName)
+    {
+        return $this->delete('/orgs/'.rawurlencode($organization).'/actions/variables/'.rawurlencode($variableName));
+    }
+
+    /**
+     * @link https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28#list-selected-repositories-for-an-organization-variable
+     *
+     * @param string $organization
+     * @param string $variableName
+     *
+     * @return array|string
+     */
+    public function selectedRepositories(string $organization, string $variableName)
+    {
+        return $this->get('/orgs/'.rawurlencode($organization).'/actions/variables/'.rawurlencode($variableName).'/repositories');
+    }
+
+    /**
+     * @link https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28#set-selected-repositories-for-an-organization-variable
+     *
+     * @param string $organization
+     * @param string $variableName
+     * @param array  $parameters
+     *
+     * @return array|string
+     */
+    public function setSelectedRepositories(string $organization, string $variableName, array $parameters = [])
+    {
+        if (!isset($parameters['selected_repository_ids'])) {
+            throw new MissingArgumentException(['selected_repository_ids']);
+        }
+
+        return $this->put('/orgs/'.rawurlencode($organization).'/actions/variables/'.rawurlencode($variableName).'/repositories', $parameters);
+    }
+
+    /**
+     * @link https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28#add-selected-repository-to-an-organization-variable
+     *
+     * @param string $organization
+     * @param string $repositoryId
+     * @param string $variableName
+     *
+     * @return array|string
+     */
+    public function addRepository(string $organization, int $repositoryId, string $variableName)
+    {
+        return $this->put('/orgs/'.rawurlencode($organization).'/actions/variables/'.rawurlencode($variableName).'/repositories/'.$repositoryId);
+    }
+
+    /**
+     * @link https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28#remove-selected-repository-from-an-organization-variable
+     *
+     * @param string $organization
+     * @param string $repositoryId
+     * @param string $variableName
+     *
+     * @return array|string
+     */
+    public function removeRepository(string $organization, int $repositoryId, string $variableName)
+    {
+        return $this->delete('/orgs/'.rawurlencode($organization).'/actions/variables/'.rawurlencode($variableName).'/repositories/'.$repositoryId);
+    }
+}
