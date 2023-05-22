490ad82f72e5a43e40c3b00849b49b8b3372ec73
Hari Darshan Gorana <er.haridarshan@gmail.com>
2023-05-22 12:49:57 +0530

    feat: add script to auto-generate `.phpstorm.meta.php` file
    Signed-off-by: Hari Darshan Gorana <er.haridarshan@gmail.com>

---
+++ generate-phpstorm-meta-file.php
@@ -0,0 +1,26 @@
+<?php
+
+use Jasny\PhpdocParser\PhpdocParser;
+use Jasny\PhpdocParser\Set\PhpDocumentor;
+
+require 'vendor/autoload.php';
+
+$reflection = new ReflectionClass(\Github\Client::class);
+$parser = new PHPDocParser(PhpDocumentor::tags());
+$meta = $parser->parse($reflection->getDocComment());
+
+$replacements = "";
+foreach($meta['methods'] as $method) {
+    $replacements .= "\"{$method['name']}\" => \Github\\{$method['return_type']}::class,".PHP_EOL;
+}
+
+$str = file_get_contents('./.phpstorm.meta.stub');
+$str = str_replace('$METHODS$', $replacements, $str);
+file_put_contents('.phpstorm.meta.1.php', $str);
+
+
+
+
+
+
+
