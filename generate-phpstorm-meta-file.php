75e7c0b185dc501ef04f26dab0a4b9e0e625d316
Hari Darshan Gorana <er.haridarshan@gmail.com>
2023-05-22 12:55:18 +0530

    chore(styleci): apply styleci patch
    Signed-off-by: Hari Darshan Gorana <er.haridarshan@gmail.com>

--- generate-phpstorm-meta-file.php
+++ generate-phpstorm-meta-file.php
@@ -9,18 +9,11 @@ $reflection = new ReflectionClass(\Github\Client::class);
 $parser = new PHPDocParser(PhpDocumentor::tags());
 $meta = $parser->parse($reflection->getDocComment());
 
-$replacements = "";
-foreach($meta['methods'] as $method) {
+$replacements = '';
+foreach ($meta['methods'] as $method) {
     $replacements .= "\"{$method['name']}\" => \Github\\{$method['return_type']}::class,".PHP_EOL;
 }
 
 $str = file_get_contents('./.phpstorm.meta.stub');
 $str = str_replace('$METHODS$', $replacements, $str);
 file_put_contents('.phpstorm.meta.1.php', $str);
-
-
-
-
-
-
-
