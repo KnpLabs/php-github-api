8a5662f5c8874b8db32c5e68d5108ff5c67d8b9f
Hari Darshan Gorana <er.haridarshan@gmail.com>
2023-05-22 13:19:11 +0530

    chore(deps): replace `jasny/phpdoc-parser` with `phpstan/phpdoc-parser` dep
    Signed-off-by: Hari Darshan Gorana <er.haridarshan@gmail.com>

--- generate-phpstorm-meta-file.php
+++ generate-phpstorm-meta-file.php
@@ -1,19 +1,28 @@
 <?php
 
-use Jasny\PhpdocParser\PhpdocParser;
-use Jasny\PhpdocParser\Set\PhpDocumentor;
+use PHPStan\PhpDocParser\Lexer\Lexer;
+use PHPStan\PhpDocParser\Parser\ConstExprParser;
+use PHPStan\PhpDocParser\Parser\PhpDocParser;
+use PHPStan\PhpDocParser\Parser\TokenIterator;
+use PHPStan\PhpDocParser\Parser\TypeParser;
 
 require 'vendor/autoload.php';
 
 $reflection = new ReflectionClass(\Github\Client::class);
-$parser = new PHPDocParser(PhpDocumentor::tags());
-$meta = $parser->parse($reflection->getDocComment());
+
+$lexer = new Lexer();
+$constExprParser = new ConstExprParser();
+$typeParser = new TypeParser($constExprParser);
+$phpDocParser = new PhpDocParser($typeParser, $constExprParser);
+
+$tokens = new TokenIterator($lexer->tokenize($reflection->getDocComment()));
+$phpDocNode = $phpDocParser->parse($tokens);
 
 $replacements = '';
-foreach ($meta['methods'] as $method) {
-    $replacements .= "\"{$method['name']}\" => \Github\\{$method['return_type']}::class,".PHP_EOL;
+foreach ($phpDocNode->getTagsByName('@method') as $node) {
+    $replacements .= "\"{$node->value->methodName}\" => \Github\\{$node->value->returnType->name}::class,".PHP_EOL;
 }
 
 $str = file_get_contents('./.phpstorm.meta.stub');
 $str = str_replace('$METHODS$', $replacements, $str);
-file_put_contents('.phpstorm.meta.1.php', $str);
+file_put_contents('.phpstorm.meta.php', $str);
