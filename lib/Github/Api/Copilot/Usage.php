e16a14c94cc1dbabe4bc4b57e328cef24061d40d
Anthony Alaan <anthony@support.webartdesign.com.au>
2024-09-03 14:45:44 +0800

    Reformat code

--- lib/Github/Api/Copilot/Usage.php
+++ lib/Github/Api/Copilot/Usage.php
@@ -8,21 +8,27 @@ class Usage extends AbstractApi
 {
     public function orgUsageSummary(string $organization, array $params = []): array
     {
-        return $this->get('/orgs/' . rawurlencode($organization) . '/copilot/usage', $params);
+        return $this->get('/orgs/'.rawurlencode($organization).'/copilot/usage', $params);
     }
 
     public function orgTeamUsageSummary(string $organization, string $teamSlug, array $params = []): array
     {
-        return $this->get('/orgs/' . rawurlencode($organization) . '/teams/' . rawurlencode($teamSlug) . '/copilot/usage', $params);
+        return $this->get(
+            '/orgs/'.rawurlencode($organization).'/teams/'.rawurlencode($teamSlug).'/copilot/usage',
+            $params
+        );
     }
 
     public function enterpriseUsageSummary(string $enterprise, array $params = []): array
     {
-        return $this->get('/enterprises/' . rawurlencode($enterprise) . '/copilot/usage', $params);
+        return $this->get('/enterprises/'.rawurlencode($enterprise).'/copilot/usage', $params);
     }
 
     public function enterpriseTeamUsageSummary(string $enterprise, string $teamSlug, array $params = []): array
     {
-        return $this->get('/enterprises/' . rawurlencode($enterprise) . '/teams/' . rawurlencode($teamSlug) . '/copilot/usage', $params);
+        return $this->get(
+            '/enterprises/'.rawurlencode($enterprise).'/teams/'.rawurlencode($teamSlug).'/copilot/usage',
+            $params
+        );
     }
 }
